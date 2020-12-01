c833f8d3a9491d57d66b069a80e38e97a8f3c0a0
kalaiyarasi-ngpws <kalaiyarasi@ngpwebsmart.com>
2020-12-01 17:20:10 +0530

    folder structure changes for github

--- github.com/api.js
+++
@@ -1,7 +0,0 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var request = new XMLHttpRequest();
-request.open("GET","http://localhost:8200/health");
-request.send();
-request.onload = () =>{
-    console.log(request);
-}
