3f656156217c37100e7361451090f223e285fd1b
kalaiyarasi-ngpws <kalaiyarasi@ngpwebsmart.com>
2021-06-21 17:25:31 +0530

    crawler exercise done

---
+++ crawler/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]bool
+}
+
+var c SafeCounter = SafeCounter{v: make(map[string]bool)}
+
+func (c *SafeCounter) Inc(key string) (bool){
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	// fmt.Println(c.v[key])
+	// fmt.Printf("%v",c.v)
+	if !c.v[key] {
+		c.v[key] = true
+		return false
+	}else {
+		return true
+	}
+	
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+	if depth <= 0 {
+		return
+	}
+	// fmt.Println("inc",c.Inc(url))
+	if c.Inc(url){
+		return
+	}
+	
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		go Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
+func main() {
+	Crawl("https://golang.org/", 4, fetcher)
+	time.Sleep(5*time.Second)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module concurrency
 
 go 1.15
 
-require golang.org/x/tour v0.1.0
+require (
+	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518
+	golang.org/x/tour v0.1.0
+)
